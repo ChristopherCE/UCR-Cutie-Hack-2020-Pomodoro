6a309a36ad8c1fd66c50a39f4943ffd3c53891f9
Chris <christopheralexman@gmail.com>
2020-11-07 15:09:06 -0800

    pomodoro class, updates

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# UCR-Cutie-Hack-2020-Pomodoro
+# UCR Cutie Hack 2020 - Pomodoro
+
+Hello!
+
+This is a project for the UCR Cutie Hack 2020.
+
+It is a Pomodoro study timer.
+
+####Hardware:
+- Raspberry Pi Zero
+- LCD Screen
+- 4 Buttons
+- so many jumper wires
+- a few resistors
+- 3D printed box -> (https://www.thingiverse.com/thing:4646388)
+    - this link will be active in 24 hours, darn
+    - I will put a picture here when it's done printing
+
+####Software:
+- Python
+- crontab, just for fun
+
+#####Resources
+- Raspberry Pi to LCD stuff
+    - https://learn.adafruit.com/character-lcds/python-circuitpython
+    
+###Requirements
+- https://learn.adafruit.com/circuitpython-on-raspberrypi-linux/installing-circuitpython-on-raspberry-pi \
+'sudo pip3 install adafruit-circuitpython-charlcd'
--- pomodoro.py
+++ pomodoro.py
@@ -1,5 +1,5 @@
 '''
-UCR Cutie Hackathon 2020
+UCR Cutie Hack 2020
 
 Pomodoro Study Timer
 
@@ -25,21 +25,14 @@ lcd_d6 = io.DigitalInOut(board.D23)
 lcd_d7 = io.DigitalInOut(board.D18)
 
 # input buttons
-btn1 = io.DigitalInOut(board.D4)
+btn0 = io.DigitalInOut(board.D4)
+btn0.direction = io.Direction.INPUT
+btn1 = io.DigitalInOut(board.D3)
 btn1.direction = io.Direction.INPUT
-
-btn2 = io.DigitalInOut(board.D3)
+btn2 = io.DigitalInOut(board.D2)
 btn2.direction = io.Direction.INPUT
-
-btn3 = io.DigitalInOut(board.D2)
+btn3 = io.DigitalInOut(board.D15)
 btn3.direction = io.Direction.INPUT
-
-btn4 = io.DigitalInOut(board.D15)
-btn4.direction = io.Direction.INPUT
-
-# test LED
-led = io.DigitalInOut(board.D14)
-led.direction = io.Direction.OUTPUT
 
 # LCD class
 lcd = charlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
@@ -48,16 +41,78 @@ lcd = charlcd.Character_LCD_Mono(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6,
 # clear LCD screen
 lcd.clear()
 
+
+class Pomodoro:
+    def __init__(self):
+        # btn_0 = 0
+        # btn_1 = 1
+        # btn_2 = 2
+        # btn_3 = 3
+        self.long_study = 1500
+        self.short_break = 300
+        self.long_break = 900
+
+    def start(self):
+        lcd.clear()
+        lcd.message = "POMODORO\nButton: 1-4"
+
+    def study(self):
+        while self.long_study:
+            mins, secs = divmod(self.long_study, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            lcd.clear()
+            lcd.message = timer
+            time.sleep(1)
+            self.long_study -= 1
+
+    def shortBreak(self):
+        while self.short_break:
+            mins, secs = divmod(self.short_break, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            lcd.clear()
+            lcd.message = timer
+            time.sleep(1)
+            self.short_break -= 1
+
+    def longBreak(self):
+        while self.long_break:
+            mins, secs = divmod(self.long_break, 60)
+            timer = '{:02d}:{:02d}'.format(mins, secs)
+            lcd.clear()
+            lcd.message = timer
+            time.sleep(1)
+            self.long_break -= 1
+
+    def reset(self):
+        lcd.clear()
+        self.long_study = 1500
+        self.short_break = 300
+        self.long_break = 900
+
+    def stop(self):
+        lcd.clear()
+
+
+def main():
+    p = Pomodoro
+    p.start()
+
+
+if __name__ == "__main__":
+    main()
+
+
+# test it up
+'''
 while True:
-	if(btn4.value == False):
-		lcd.clear()
-		lcd.message = "BUTTON ON"
-		print("BUTTON ON")
-		led.value = True
-		time.sleep(0.2)
-	else:
-		lcd.clear()
-		lcd.message = "BUTTON OFF"
-		print("BUTTON OFF")
-		led.value = False
-		time.sleep(0.2)
+    if btn0.value is False:
+        lcd.clear()
+        lcd.message = "BUTTON ON"
+        print("BUTTON ON")  # debugging
+        time.sleep(0.2)
+    else:
+        lcd.clear()
+        lcd.message = "BUTTON OFF"
+        print("BUTTON OFF")  # debugging
+        time.sleep(0.2)
+'''
